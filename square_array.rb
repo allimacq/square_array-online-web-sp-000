42b205f1fe18f27f9618d1d39228260f8fddea53
Allison MacQueen <allimacq@gmail.com>
2020-08-28 18:55:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   square = [ ]
   array.each do |int| 
-    square(int - 1) = int ** 2
+    square[int - 1] = int ** 2
     #square = Array.new(array){|int| int ** 2}
     puts square
  end
