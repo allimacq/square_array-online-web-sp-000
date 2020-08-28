e95232cb2696faf59dd8d3faa7cb6518c96ed3c4
Allison MacQueen <allimacq@gmail.com>
2020-08-28 18:33:54 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,3 @@
 def square_array(array)
-  array.each do |int|
-    i = 3
-    square = Array.new(3)
-    for i in (0..2)
-      square[i] = int ** 2
-    end
-    puts square
-  end
+  array.each { |i| return i ** 2}
 end
