98761be3fe902d893774ed76b29b8f1a5d9cc3b2
Allison MacQueen <allimacq@gmail.com>
2020-08-28 18:49:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  square = Array.new(array)
+  #square = Array.new(array)
   array.each do |int| 
-   square = Array.new(array){|int| int ** 2}
-   return square
+    square = Array.new(3){|int| int ** 2}
+    return square
  end
 end
