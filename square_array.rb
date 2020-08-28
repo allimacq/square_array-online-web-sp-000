0b82c0e19e64b1be6b668dcdd9653a7470d86124
Allison MacQueen <allimacq@gmail.com>
2020-08-28 19:12:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,14 +2,8 @@ def square_array(array)
   square = Array.new()
   i = 0
   array.each do |int| 
-    #the problem is int -1. need a better index, like the actual one (0,1,2,3,etc)
     square[i] = int ** 2
-    #square = Array.new(array){|int| int ** 2}
-    puts square
     i += 1
-    puts i
  end
- puts "OUT OF LOOP"
- puts square
  return square
 end
