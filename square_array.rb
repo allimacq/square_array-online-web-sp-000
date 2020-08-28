973c5529efc70d32c277fd95ec55d867b792fe79
Allison MacQueen <allimacq@gmail.com>
2020-08-28 19:02:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
   square = Array.new(3)
   array.each do |int| 
+    
+    #the problem is int -1. need a better index, like the actual one (0,1,2,3,etc)
     square[int - 1] = int ** 2
     #square = Array.new(array){|int| int ** 2}
     puts square
